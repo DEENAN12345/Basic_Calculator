beb5355130bb605fd88dadd55ad42714fc07924b
DEENAN12345 <118874946+DEENAN12345@users.noreply.github.com>
2023-07-13 14:58:40 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,11 @@
+var a=parseInt(prompt("Enter a number"));
+var b=parseInt(prompt("Enter a number"));
+var op=prompt("Enter the operator");
+switch(op)
+{
+    case '+':document.getElementById("result").textContent=(a+b);break;
+    case '-':document.getElementById("result").textContent=(a-b);break;
+    case '*':document.getElementById("result").textContent=(a*b);break;
+    case '/':document.getElementById("result").textContent=(a/b);break;
+}
+
